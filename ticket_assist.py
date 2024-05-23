397c7fbb530d568afce10df90781b0e34cde222e
SRshohan <srshohan02@gmail.com>
2024-05-22 20:59:46 -0400

    Splitting text

--- ticket_assist.py
+++ ticket_assist.py
@@ -1,4 +1,3 @@
-import re
 from dotenv import load_dotenv
 import os
 from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
@@ -20,45 +19,42 @@ def load_llm():
     return llm
 
 def template_of(question):
-    template = """I want you to act like professional IT expert but give answer based on what i am looing for and guide me through daily IT problems or emails. Like question I "dont Understand what that mean", "can you help me understand" without yapping and precise respond. I will peovide email or a problem to trouble shoot: {question}
+
+    data = {}
+
+    template1 = """I want you to act like professional IT expert and client service expert but give answer based on what i am looing for and guide me through daily IT problems or emails. Like question I "dont Understand what that mean", "can you help me understand" without yapping and precise respond. I will peovide email or a problem to trouble shoot: {question}
 
     Answer: Let's think of the problem in fundamental level and high level first. 
-    1. Explain possible reason behind it with bullet points precise explaination. 
-    2. Ask for possible follow question (Give multiple option). Then Ask related questions to more specify the service or to the problem has been reffered.
-    3. Then, Suggest me a professional reply for the ticketing system update.  
-    Make sure couple things for reply: Hi and full name of the person. Thanks the person for reaching out for first time respond. Sign my name Sohanur. And Specific and exact title for each section.
-    (Write title as "1. Reasoning:", "2. Options and Questions: ", "3. Professional Response:") be consistent with title and colon sign
-    """
-   
-
-    prompt = PromptTemplate.from_template(template)
-
-    chain = prompt | load_llm()
-
-#     question = """HI, there,
-# this is a follow-up to the advice Kevin Clancy gave (see below) about asking for your guidance to save the emails I'd like to keep before having my computer(s) wiped. 
-
-# Many thanks, as always.
-# Sister Remigia, Ask like questions, she want to backup all the emails in drive or related to it"""
-    return chain.invoke({"question": question})
-
-def split_output(text):
-    # separation = {}
-
+    1. Explain possible reason behind it with bullet points precise explaination. """
     
-    data = {}
-    if '1. Reasoning:' in text:
-        data['reason'] = text[0:text.index('**2.')]
+    template2 =""" I will feed some data and I want you act like expereience client service expert (Usually IT) .Ask for possible follow question (Give multiple option). Then Ask related questions to more specify the service or to the problem has been reffered. {question}"""
+    
+    template3 ="""  I will feed some data and I want you act like expereience client service expert (Usually IT). Then, Suggest me a professional reply for the ticketing system update. Hi and full name of the person. Thanks the person for reaching out for first time respond. Sign my name Sohanur. {question} """
 
-    elif '2. Options and Questions:' in text:
-        data['options'] = text[text.index('**2.'):text.index('**3.')]
+    prompt1 = PromptTemplate.from_template(template1)
+    prompt2 = PromptTemplate.from_template(template2)
+    prompt3 = PromptTemplate.from_template(template3)
 
-    # return separation['Response']
+    chain1 = prompt1 | load_llm()
+    chain2 = prompt2 | load_llm()
+    chain3 = prompt3 | load_llm()
+
+    c1 = chain1.invoke({"question": question})
+    c2 = chain2.invoke({"question": question})
+    c3 = chain3.invoke({"question": question})
+
+    data["reason"] = c1
+    data["options"] = c2
+    data["response"] = c3
+
+
     return data
+
+
 
 
 if __name__ == '__main__':
     question = "Hello, I have a second monitor that suddenly stopped connecting to my laptop and I'm struggling to figure out why. Could someone stop by this week to assist? I am available between 2-4PM today and between Noon and 3:30 PM tomorrow. Thank you! Best, Chloe"
     generated = template_of(question)
-    print(generated)
-    print(split_output(generated))
+    print(generated['reason'])
+    
